250550bb1b17a00a8dae417af880008103e12787
Ian Cross <crossia@mnstate.edu>
2016-02-24 10:10:18 -0600

    created test for createMenu, added doc

---
+++ testMenu.py
@@ -0,0 +1,12 @@
+from createMenu import *
+choiceListA = [ 'Add Employee', 'Delete Employee', 'Find Employee', 'Change Employee', 'Print Roster', 'Quit']
+a = createMenu(choiceListA)
+print(a)
+
+choiceListB = [ 'Checkings', 'Savings', 'Withdraw', 'Transfer', 'Quit']
+b = createMenu(choiceListB)
+print(b)
+
+choiceListC = [ 'Add Student', 'Remove Student', 'Print Student List', 'Quit']
+c = createMenu(choiceListC)
+print(c)
